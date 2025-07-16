9517ab77b083a33311dea031eb95e79b9dce9338
Jonas <lindinger.jonas.yt@gmail.com>
2025-07-16 20:41:23 +0200

    add: improtant coding comment

--- JsonReader.py
+++ JsonReader.py
@@ -4,6 +4,7 @@ def Read_Json_File(fileName: str):
     dataToReturn = None
 
     try:
+        # "r" -> open for reading (default)
         with open(fileName, "r", encoding="utf-8-sig") as file:
             data = json.load(file)
             dataToReturn = data
